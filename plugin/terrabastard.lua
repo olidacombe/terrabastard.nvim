327d40e1d905fe197977b0859d6d0879b3610ec7
Oli Dacombe <olidacombe@gmail.com>
2024-02-15 22:35:52 +0000

    try fix

--- plugin/terrabastard.lua
+++ plugin/terrabastard.lua
@@ -4,4 +4,4 @@ local user_command_prefix = "Terrabastard"
 
 vim.api.nvim_create_user_command(user_command_prefix .. "ExtractIamPolicy", function(opts)
     terrabastard.extract_hard_policy()
-end)
+end, {})
